9cc4cfa75753ab6880aeabe34f25a7c56c8a48ec
Maxime Coriton <maxime.coriton@gmail.com>
2022-08-11 00:01:35 +0200

    field

--- pages/addpost.vue
+++ pages/addpost.vue
@@ -2,27 +2,20 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
 
-
-
       <v-card>
         <v-toolbar flat color="blue-grey" dark>
           <v-toolbar-title>Ajouter un nouveau post</v-toolbar-title>
         </v-toolbar>
 
-
-
         <v-card-text>
-          <v-text-field filled label="Titre" ></v-text-field>
+          <v-text-field filled label="Titre"></v-text-field>
+          <v-text-field filled label="Localisation"></v-text-field>
 
           <input type="file" accept="image/gif, image/jpeg, image/png"
             onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])">
           <img id="blah" width="100%" height="100%" />
 
-          <v-textarea filled label="Description">
-          </v-textarea>
-
-
-
+          <v-textarea filled label="Description"></v-textarea>
         </v-card-text>
 
         <v-divider></v-divider>
@@ -45,18 +38,10 @@
 <script>
 
 
+
 export default {
-
-  name: 'AddPostPage',
-  middleware: "auth",
-
-
-
-  methods: {
-    signOut() {
-      this.$fire.auth.signOut();
-      window.location = "/login"
-    }
-  },
+    name: "AddPostPage",
+    middleware: "auth",
+    
 }
 </script>
